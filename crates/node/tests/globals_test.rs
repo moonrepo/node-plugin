87d9d538b736e31fd9990b8df05f6ede8a12fe94
Miles Johnson <mileswjohnson@gmail.com>
2023-08-22 15:04:34 -0700

    Comment out test.

--- crates/node/tests/globals_test.rs
+++ crates/node/tests/globals_test.rs
@@ -1,4 +1,4 @@
-use proto_pdk_test_utils::*;
-use starbase_sandbox::create_empty_sandbox;
+// use proto_pdk_test_utils::*;
+// use starbase_sandbox::create_empty_sandbox;
 
-generate_globals_test!("node-test", "prettier");
+// generate_globals_test!("node-test", "prettier");
