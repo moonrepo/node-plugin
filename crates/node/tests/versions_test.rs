a56976b2ab1447674e14607e7e5709c9a8525791
Miles Johnson <mileswjohnson@gmail.com>
2023-09-08 16:20:39 -0700

    Update tests.

--- crates/node/tests/versions_test.rs
+++ crates/node/tests/versions_test.rs
@@ -7,7 +7,7 @@ generate_resolve_versions_tests!("node-test", {
     "lts-gallium" => "16.20.2",
     "lts/fermium" => "14.21.3",
     "stable" => "18.17.1",
-    "node" => "20.5.1",
+    "node" => "20.6.1",
 });
 
 #[test]
